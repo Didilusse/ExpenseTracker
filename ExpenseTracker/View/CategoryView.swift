d33a2861451233af8e525dc01b957a2a20f25869
Adil Rahmani <didilusse@gmail.com>
2024-04-07 13:10:28 -0400

    commit

---
+++ ExpenseTracker/View/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  ExpenseTracker
+//
+//  Created by Adil Rahmani on 4/7/24.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
---
+++ ExpenseTracker/View/ExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseView.swift
+//  ExpenseTracker
+//
+//  Created by Adil Rahmani on 4/7/24.
+//
+
+import SwiftUI
+
+struct ExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseView()
+}
