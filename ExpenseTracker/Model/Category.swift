ae8d64f9dabf5eac91a13b1822c17ed5d058005c
Adil Rahmani <didilusse@gmail.com>
2024-04-07 10:57:25 -0400

    Added to needed models for the app

---
+++ ExpenseTracker/Model/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  ExpenseTracker
+//
+//  Created by Adil Rahmani on 4/7/24.
+//
+
+import SwiftUI
+
+struct Category: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Category()
+}
---
+++ ExpenseTracker/Model/Expense.swift
@@ -0,0 +1,18 @@
+//
+//  Expense.swift
+//  ExpenseTracker
+//
+//  Created by Adil Rahmani on 4/7/24.
+//
+
+import SwiftUI
+
+struct Expense: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Expense()
+}
