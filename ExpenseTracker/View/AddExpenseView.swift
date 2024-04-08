68d6533b8da5b5b758148989e4f4b665061ed61f
Adil Rahmani <didilusse@gmail.com>
2024-04-08 12:36:29 -0400

    Made the + button functional

---
+++ ExpenseTracker/View/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  ExpenseTracker
+//
+//  Created by Adil Rahmani on 4/8/24.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
--- ExpenseTracker/View/ExpenseView.swift
+++ ExpenseTracker/View/ExpenseView.swift
@@ -15,6 +15,7 @@ struct ExpensesView: View {
     ], animation: .snappy) private var allExpenses: [Expense]
     //Grouped Expenses
     @State private var groupedExpenses: [GroupedExpense] = []
+    @State private var addExpense: Bool = false
     var body: some View {
         NavigationStack{
             List{
@@ -32,12 +33,48 @@ struct ExpensesView: View {
             .toolbar{
                 ToolbarItem(placement: .topBarTrailing){
                     Button{
-                        
+                        addExpense.toggle()
                     }label: {
                         Image(systemName: "plus.circle.fill")
                             .font(.title3)
                     }
                 }
+            }
+        }
+        .onChange(of: allExpenses, initial: true) { oldValue, newValue in if groupedExpenses.isEmpty{
+                createGroupedExpenses(newValue)
+            }
+        }
+        .sheet(isPresented: $addExpense, content: {
+            AddExpenseView()
+        })
+    }
+    /// Creating Grouped Expenses (Grouping By Date)
+    func createGroupedExpenses(_ expenses: [Expense]) {
+        Task.detached(priority: .high) {
+            let groupedDict = Dictionary(grouping: expenses) { expense in
+                let dateComponents = Calendar.current.dateComponents([.day, .month, .year], from: expense.date)
+                
+                return dateComponents
+            }
+            
+            /// Sorting Dictionary in Descending Order
+            let sortedDict = groupedDict.sorted {
+                let calendar = Calendar.current
+                let date1 = calendar.date(from: $0.key) ?? .init()
+                let date2 = calendar.date(from: $1.key) ?? .init()
+                
+                return calendar.compare(date1, to: date2, toGranularity: .day) == .orderedDescending
+            }
+            
+            /// Adding to the Grouped Expenses Array
+            /// UI Must be Updated on Main Thread
+            await MainActor.run {
+                groupedExpenses = sortedDict.compactMap({ dict in
+                    let date = Calendar.current.date(from: dict.key) ?? .init()
+                    return .init(date: date, expenses: dict.value)
+                })
+                
             }
         }
     }
